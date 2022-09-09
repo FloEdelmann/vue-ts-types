03c83fff50f9b01f01fd63845376c1a586764973
Flo Edelmann <florian-edelmann@online.de>
2022-09-09 16:07:03 +0200

    Narrow type parameter in type test

--- type-tests/utils.ts
+++ type-tests/utils.ts
@@ -1,8 +1,9 @@
 import Vue from 'vue2-6';
+import type { PropOptions } from 'vue2-6/types/options';
 import type { ExtendedVue } from 'vue2-6/types/vue';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const createVue2Component = <T>(prop: T) => Vue.component('test', {
+export const createVue2Component = <T extends PropOptions>(prop: T) => Vue.component('test', {
   props: { prop },
 });
 
