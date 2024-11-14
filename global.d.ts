ca4172d5119ced820b991fdf8f6bad8298c57fb9
Flo Edelmann <git@flo-edelmann.de>
2024-11-14 13:08:00 +0100

    Migrate config

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-import type CustomMatchers from 'jest-extended';
-
-declare module 'expect' {
-  interface AsymmetricMatchers extends CustomMatchers {}
-  interface Matchers<R> extends CustomMatchers<R> {}
-}
