4eb52d8302175e5abb5b222bdf290b41f1b88a8a
Flo Edelmann <git@flo-edelmann.de>
2026-06-15 17:01:15 +0200

    Inline-disable `unicorn/prefer-number-is-safe-integer` lint rule

--- src/validators/isInteger.ts
+++ src/validators/isInteger.ts
@@ -2,6 +2,7 @@ import type { Validator } from '.';
 
 /** Validator that only allows integer numbers. */
 export const isInteger: Validator = (value) => {
+  // eslint-disable-next-line unicorn/prefer-number-is-safe-integer -- validator function should match expectation
   if (typeof value !== 'number' || !Number.isInteger(value)) {
     return 'value should be an integer';
   }
